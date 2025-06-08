958d34258a9e35f18c2b7993c4ed9612df659f62
Leopold Cesbron <lcesbron@student.42lyon.fr>
2025-06-08 15:14:54 +0200

    42 header plugin + <leader>so + <leader>st

--- init.lua
+++ init.lua
@@ -434,6 +434,10 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+      vim.keymap.set('n', '<leader>st', builtin.colorscheme, { desc = '[S]earch [T]heme' })
+      vim.keymap.set('n', '<leader>so', function()
+        vim.cmd 'TodoTelescope'
+      end, { desc = '[S]earch T[O]dos' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -10,4 +10,19 @@ return {
     },
     ft = 'c',
   },
+  {
+    'kporceil/42lyon-header.nvim',
+    cmd = { 'Stdheader' },
+    keys = { '<F1>' },
+    opts = {
+      default_map = true, -- Default mapping <F1> in normal mode.
+      auto_update = true, -- Update header when saving.
+      user = 'lcesbron', -- Your user.
+      mail = 'lcesbron@student.42lyon.fr', -- Your mail.
+      -- add other options.
+    },
+    config = function(_, opts)
+      require('42header').setup(opts)
+    end,
+  },
 }
